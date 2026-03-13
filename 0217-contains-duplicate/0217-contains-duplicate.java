063a3080ffe90c115e9c5b06de5b0ca8f95d934e
Shirisha mangenapally <mangenapallyshirisha123@gmail.com>
2026-03-13 18:33:16 +0530

    Time: 15 ms (65.16%), Space: 93.6 MB (46.91%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> res=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(res.contains(nums[i])) return true;
+            res.add(nums[i]);
+        }
+        return false;
+    }
+}
