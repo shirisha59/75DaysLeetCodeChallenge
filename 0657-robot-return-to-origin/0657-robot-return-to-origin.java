3468f3c16cac8d23eb7225cbd35bfbe0759e0ab7
Shirisha mangenapally <mangenapallyshirisha123@gmail.com>
2026-04-05 09:42:35 +0530

    Time: 7 ms (30.8%), Space: 45.5 MB (75.15%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -4,10 +4,10 @@ class Solution {
         int y=0;
         int len=moves.length();
         for(int i=0;i<len;i++){
-            if(moves.charAt(i)=='L') x=x+1;
-            else if(moves.charAt(i)=='R') x=x-1;
-            else if(moves.charAt(i)=='U') y=y+1;
-            else y=y-1;
+            if(moves.charAt(i)=='L') x++;
+            else if(moves.charAt(i)=='R') x--;
+            else if(moves.charAt(i)=='U') y++;
+            else y--;
         }
         return x==0 && y==0;
     }
