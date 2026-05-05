f10e7a55a5fa7553a73a5a1e02fa5b5d012b8e37
Shirisha mangenapally <mangenapallyshirisha123@gmail.com>
2026-05-05 18:55:55 +0530

    Time: 1 ms (99.97%), Space: 47.7 MB (80.17%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+       int n=nums.length;
+       int last_pos=n-1;
+       for(int i=n-1;i>=0;i--){
+        if(i+nums[i]>=last_pos) last_pos=i;
+       }
+       return last_pos==0;
+        
+    }
+}
