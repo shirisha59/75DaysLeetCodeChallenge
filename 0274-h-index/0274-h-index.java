1d053456ffe3eb278313323bb61c7da5815887d9
Shirisha mangenapally <mangenapallyshirisha123@gmail.com>
2026-05-05 21:18:03 +0530

    Time: 5 ms (68.26%), Space: 43.6 MB (43.45%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,11 +1,11 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int n=citations.length,i;
+        int n=citations.length;
+        int i;
         Arrays.sort(citations);
         for(i=1;i<=n;i++){
-            if(i>citations[n-i]) break;  
+            if(i>citations[n-i]) break;
         }
         return i-1;
-        
     }
 }
