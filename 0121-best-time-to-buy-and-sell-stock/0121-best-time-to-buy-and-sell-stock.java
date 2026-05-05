052dcf95d165ae43ed4d54428ef2f3c8bb76d048
Shirisha mangenapally <mangenapallyshirisha123@gmail.com>
2026-05-05 18:49:32 +0530

    Time: 2 ms (39.11%), Space: 93.6 MB (96.8%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,19 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        
         int max_profit=0;
         int buy_price=prices[0];
         int n=prices.length;
         for(int i=1;i<n;i++){
-            int cur_profit=prices[i]-buy_price;
-            if(cur_profit>max_profit){
-                max_profit=cur_profit;
-            }
-            if(prices[i]<buy_price){
-                buy_price=prices[i];
-            }
+            int curr_profit=prices[i]-buy_price;
+            if(curr_profit>max_profit) max_profit=curr_profit;
+            if(prices[i]<buy_price) buy_price=prices[i];
         }
         return max_profit;
-        
     }
 }
